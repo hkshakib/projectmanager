3283a5d071135b9b894c8421444cbb63d33817b0
hkshakib <humayunkibriashakib@gmail.com>
2023-07-16 23:31:07 +0600

    feat: MUI Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Layout/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">hello</div>
+      <Navbar/>
+      <div className="App"></div>
     </BrowserRouter>
   );
 };
