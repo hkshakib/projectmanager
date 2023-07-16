0ac0d4a6700b21b018e8ef7a62f084e6c5cc101c
hkshakib <humayunkibriashakib@gmail.com>
2023-07-16 20:37:30 +0600

    feat: Installed Necessary Packeges: MUI, React-Router Dom, Redux, React-Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+      <div className="App">hello</div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
