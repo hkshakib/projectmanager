8c81573ece327101f4a8894dea55664f30e0205b
hkshakib <humayunkibriashakib@gmail.com>
2023-07-17 15:23:00 +0600

    feat: Added SignIn, SignUp, Create Project Pages and Corrosponding Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import ProjectDetails from "./Components/Projects/Projectdetails";
+import SignIn from "./Components/Auth/SignIn";
+import SignUp from "./Components/Auth/SignUp";
+import CreateProject from "./Components/Projects/CreateProject";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" Component={Dashboard}/>
         <Route path="/project/:id" Component={ProjectDetails}/>
+        <Route path="/signin" Component={SignIn}/>
+        <Route path="/signup" Component={SignUp}/>
+        <Route path="/create" Component={CreateProject}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Projects/CreateProject.jsx
@@ -0,0 +1,65 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+const CreateProject = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Create New Project
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="title"
+            label="Project Title"
+            name="Title"
+            autoComplete="Title"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="description"
+            label="Description"
+            type="text"
+            id="description"
+            autoComplete="description"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default CreateProject;
