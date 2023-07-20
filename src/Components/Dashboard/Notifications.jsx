95a2927a8630743bf876209b1b470573b6f42113
hkshakib <humayunkibriashakib@gmail.com>
2023-07-20 16:26:33 +0600

    feat: Acces Store from Notification and setup basic skeleton

--- src/Components/Dashboard/Notifications.jsx
+++ src/Components/Dashboard/Notifications.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+const getTimeDifference = (time) => {
+  const currentTime = new Date();
+  const postedTime = new Date(time);
+  const timeDiffInMilliseconds = currentTime - postedTime;
+  const seconds = Math.floor(timeDiffInMilliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  if (hours > 0) {
+    return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
+  } else {
+    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
+  }
+};
 
 const Notifications = () => {
-  return <div>Notifications</div>;
+  const data = useSelector((state) => state.projects.projects);
+  const len = data.length;
+  const Project = len > 0 ? data[len - 1] : null;
+
+  if (!Project || !Project.project) {
+    return <div>No Notification available</div>;
+  }
+  const TimeDifference = getTimeDifference(Project.project.createTime);
+
+  return (
+    <div>
+      <div>{Project.project.title}</div>
+      <div>
+        {Project.project.email} posted {TimeDifference}
+      </div>
+    </div>
+  );
 };
 
 export default Notifications;
