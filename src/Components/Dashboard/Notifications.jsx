ce3615b40e6cb053a14a8146d6236fe2ceb676c0
hkshakib <humayunkibriashakib@gmail.com>
2023-07-25 14:41:51 +0600

    feat: Added Modal for Alart on Delete project

--- src/Components/Dashboard/Notifications.jsx
+++ src/Components/Dashboard/Notifications.jsx
@@ -6,8 +6,6 @@ import PropTypes from "prop-types";
 
 import {
   Box,
-  Card,
-  CardContent,
   Typography,
   Tabs,
   Tab,
--- src/Components/Projects/ProjectSummery.jsx
+++ src/Components/Projects/ProjectSummery.jsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import {
   DeleteProject,
@@ -15,21 +14,41 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Modal,
+  Box,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import CollapseMoreIcon from "@mui/icons-material/CollapseMore";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "1px solid gray",
+  boxShadow: 1,
+  p: 4,
+};
 
 const ProjectSummery = ({ value, id }) => {
-  const createdAt = new Date(value.project.createTime).toLocaleString();
-  console.log(id);
+  const [open, setOpen] = useState(null);
+  const handleOpen = () => setOpen(id);
+  const handleClose = () => setOpen(null);
 
+  const { title, email, createTime, content } = value.project;
+  const createdAt = new Date(createTime).toLocaleString();
   const dispatch = useDispatch();
-  const handleDelete = (id) => {
-    let project = {
+
+  const handleDelete = () => {
+    const project = {
       ...value,
       deletedTime: new Date().toISOString(),
     };
     dispatch(DeleteProject(id));
     dispatch(DeletedProject(project));
+    handleClose();
   };
 
   return (
@@ -37,48 +56,60 @@ const ProjectSummery = ({ value, id }) => {
       <Accordion sx={{ margin: 1 }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
+          // collapseIcon={<CollapseMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>{value.project.title}</Typography>
+          <Typography>{title}</Typography>
         </AccordionSummary>
 
         <AccordionDetails>
           <Card variant="text">
             <CardContent>
-              <Typography
-                variant="h4"
-                component="div"
-                sx={{ display: "flex", justifyContent: "start" }}
-              >
-                {value.project.title}
+              <Typography variant="h4" component="div">
+                {title}
               </Typography>
-              <Typography
-                color="text.secondary"
-                sx={{ mb: 1.5, display: "flex", justifyContent: "start" }}
-              >
-                Posted By {value.project.email} at {createdAt}
+              <Typography color="text.secondary" sx={{ mb: 1.5 }}>
+                Posted By {email} at {createdAt}
               </Typography>
-              <Typography
-                variant="body2"
-                sx={{ display: "flex", justifyContent: "start" }}
-              >
-                {value.project.content}
-              </Typography>
+              <Typography variant="body2">{content}</Typography>
             </CardContent>
             <CardActions>
               <Button size="small">More</Button>
-              <Button
-                size="small"
-                color="error"
-                onClick={() => handleDelete(id)}
-              >
+              <Button size="small" color="error" onClick={handleOpen}>
                 Delete
               </Button>
             </CardActions>
           </Card>
         </AccordionDetails>
       </Accordion>
+      <Modal
+        open={open === id}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="p"
+            fontSize={16}
+          >
+            Do you want to delete this project? This Can't Be undone!
+          </Typography>
+          <Typography
+            sx={{ display: "flex", justifyContent: "center", pt: 8, gap: 4 }}
+          >
+            <Button variant="outlined" color="primary" onClick={handleClose}>
+              NO
+            </Button>
+            <Button variant="outlined" color="error" onClick={handleDelete}>
+              YES
+            </Button>
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   );
 };
