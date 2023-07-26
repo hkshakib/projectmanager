e2d5ff4dea2d1dd606b21207e2dc3fc42d01ddbb
hkshakib <humayunkibriashakib@gmail.com>
2023-07-26 17:52:30 +0600

    feat: Added Needed Options for project creations

--- src/Components/Projects/CreateProject.jsx
+++ src/Components/Projects/CreateProject.jsx
@@ -7,7 +7,21 @@ import { AddProject } from "../../Store/Reducers/ProjectReducer";
 
 import { getAuth } from "firebase/auth";
 
-import { Button, TextField, Box, Typography, Container } from "@mui/material";
+import {
+  Button,
+  TextField,
+  Box,
+  Typography,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import MiniDrawer from "../Layout/MiniDrawer";
 
 const CreateProject = () => {
   const dispatch = useDispatch();
@@ -15,6 +29,9 @@ const CreateProject = () => {
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [objective, setObjective] = useState("");
+  const [deadline, setDeadline] = useState(null);
+  const [priority, setPriority] = useState("");
 
   const Auth = getAuth();
 
@@ -24,17 +41,24 @@ const CreateProject = () => {
       email: Auth.currentUser.email,
       title,
       content,
+      objective,
       createTime: new Date().toISOString(),
+      deadline: deadline ? deadline.toISOString() : null,
+      priority,
     };
     console.log(project);
     dispatch(AddProject(project));
     setTitle("");
     setContent("");
+    setDeadline(null);
+    setPriority("");
+    setObjective("");
     navigate("/");
   };
 
   return (
     <Container component="main" maxWidth="xs">
+      <MiniDrawer />
       <Box
         sx={{
           marginTop: 8,
@@ -67,6 +91,7 @@ const CreateProject = () => {
             margin="normal"
             required
             fullWidth
+            multiline
             name="description"
             label="Description"
             type="text"
@@ -74,6 +99,47 @@ const CreateProject = () => {
             autoComplete="description"
             onChange={(e) => setContent(e.target.value)}
           />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            multiline
+            name="objective"
+            label="Objective"
+            type="text"
+            id="description"
+            autoComplete="objective"
+            onChange={(e) => setObjective(e.target.value)}
+          />
+          <FormControl fullWidth sx={{ mt: 1, mb: 1 }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                format="MM/DD/YYYY"
+                label="Dead Line *"
+                value={deadline}
+                onChange={(newDeadLine) => setDeadline(newDeadLine)}
+                slotProps={{ textField: { placeholder: "MM/DD/YYYY" } }}
+                required
+              />
+            </LocalizationProvider>
+          </FormControl>
+
+          <FormControl fullWidth sx={{ mt: 1 }}>
+            <InputLabel>Set Priority *</InputLabel>
+            <Select
+              value={priority}
+              label="Set Priority"
+              onChange={(e) => setPriority(e.target.value)}
+              required
+            >
+              {["LOW", "MEDIUM", "HIGH"].map((text, index) => (
+                <MenuItem value={text} key={index} sx={{ fontSize: 14 }}>
+                  {text}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
           <Button
             type="submit"
             fullWidth
--- src/Components/Projects/Projectdetails.jsx
+++ src/Components/Projects/Projectdetails.jsx
@@ -1,26 +1,59 @@
 import React from "react";
 import { Box, Card, CardContent, Typography } from "@mui/material";
+
+import { LineChart } from "@mui/x-charts/LineChart";
+
 import { useSelector } from "react-redux";
 
 import { useParams } from "react-router-dom";
+import MiniDrawer from "../Layout/MiniDrawer";
 
 const ProjectDetails = () => {
   const projects = useSelector((state) => state.projects.projects);
   const { id } = useParams();
 
   const project = projects.find((p) => p.id === id);
-  console.log(project, id);
+
   return (
     <Box
       sx={{
         minWidth: 275,
         display: "flex",
-        flexDirection: "column",
-        justifyContent: "start",
-        mb: 1.5,
-        pt: 1.5,
+        // flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        m: 1.5,
+        p: 1.5,
+        gap: 4,
       }}
     >
+      <MiniDrawer />
+      <Card variant="outlined">
+        <CardContent>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{ display: "flex", justifyContent: "start" }}
+          >
+            Dependencies
+          </Typography>
+          <Typography
+            color="text.secondary"
+            sx={{
+              mb: 1.5,
+              display: "flex",
+              justifyContent: "start",
+              flexDirection: "column",
+            }}
+          >
+            <Typography>1) Install DevTools</Typography>
+            <Typography>2) Install DevTools</Typography>
+            <Typography>3) Install DevTools</Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+
       <Card variant="outlined">
         <CardContent>
           <Typography
@@ -51,6 +84,72 @@ const ProjectDetails = () => {
           </Typography>
         </CardContent>
       </Card>
+
+      <Card variant="outlined">
+        <CardContent>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{ display: "flex", justifyContent: "start" }}
+          >
+            Dependencies
+          </Typography>
+          <Typography
+            color="text.secondary"
+            sx={{
+              mb: 1.5,
+              display: "flex",
+              justifyContent: "start",
+              flexDirection: "column",
+            }}
+          >
+            <Typography>1) Install DevTools</Typography>
+            <Typography>2) Install DevTools</Typography>
+            <Typography>3) Install DevTools</Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <Card variant="outlined">
+        <CardContent>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{ display: "flex", justifyContent: "start" }}
+          >
+            Dependencies
+          </Typography>
+          <Typography
+            color="text.secondary"
+            sx={{
+              mb: 1.5,
+              display: "flex",
+              justifyContent: "start",
+              flexDirection: "column",
+            }}
+          >
+            <Typography>1) Install DevTools</Typography>
+            <Typography>2) Install DevTools</Typography>
+            <Typography>3) Install DevTools</Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+      
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        width={300}
+        height={300}
+        sx={{
+          border: "1px solid black",
+          backgroundColor: "white",
+          flexGrow: 0,
+        }}
+      />
     </Box>
   );
 };
